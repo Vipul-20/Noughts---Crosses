89b3dbdf95748909c639458a019624b16a049b20
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-09-23 14:31:45 +0000

    add some function

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,30 @@
-// const resetBtn = document.querySelector(".btn-reset");
-// resetBtn.style.background = "red";
+const headingEl = document.querySelector(".heading");
+const currentPlayer = document.querySelector(".player-stutus");
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector(".btn-reset");
 
-// const resetBt = document.querySelector(".btn-reset");
-// const gameGrid = document.querySelector(".game-grid");
+// ======Game Variable=====
+const players = ["O", "X"];
+let currPlayer = "O";
 
-// resetBt.addEventListener("click", function () {
-//   //   console.log("hi");
-//   alert("if you want to change the color");
-//   gameGrid.style.backgroundColor = "red";
-// });
+function getPlayer() {
+  const rn = Math.floor(Math.random() * 2);
+  return players[rn];
+}
 
-// resetBt.addEventListener("click", function () {
-//   resetBt.classList.remove("active");
-// });
-// const color = ["orange", "green", "red", "yellow", "purple"];
+function swapPlayer() {
+  currPlayer = currPlayer === "O" ? "X" : "O";
+}
 
-// let count = 0;
-// resetBt.addEventListener("click", function () {
-//   if (count > 5) {
-//     count = count - 6;
-//   }
+// only the first one
+function handalClick(input) {
+  input.textContent = currPlayer;
+  swapPlayer();
+  currPlayerEl.textContent = currPlayer;
+}
 
-//   gameGrid.style.backgroundColor = color[count];
-//   count = count + 1;
-// });
-// const fruit = ["apple", "mango", "gava", "bhajiya", "samosa"];
-// // push,pop,shift,unshift
-// const x = fruit.pop();
-// console.log(x);
-
-// const fruit = ["apple", "mango", "gava", "bhajiya", "samosa"];
-// const x = fruit.join("_")
-// console.log(x)
-
-// function table(num) {
-//   for (let i = 0; i <= 10; i *= 2) {
-//     // console.log(i)
-//     console.log(num + "x" + i + "=" + num * i);
-//   }
-// }
-// table(3);
-
-// const num=[2,4,6,8,9]
-// for (let j=0; j<num.length;j++){
-//   num[j]=num[j]*2
-// }
-// console.log(num)
-// const num=[2,4,6,8,9]
-// num.forEach(function(a){
-// console.log(a*2)
-// });
-// const nums=[2,4,6,8,9]
-// nums.forEach(function(nums,index){
-// nums[index]=nums*2
-// });
-// console.log(nums)
-
-const nums=[2,4,6,8,9]
-// numsEven=[false,true,true,false]
-
-// const x=nums.map(function(){
-//   return 1;
-// })
-// console.log(x)
-
-// const x=nums.map(function(value){
-// return value % 2 ==0;
-// })
-// console.log(x)
-const numsEven=[];
-const x=nums.map(function(value){
-  return value % 2 ==0;
-  })
-  console.log(x)
+boxes.forEach(function (box) {
+  box.addEventListener("click", function () {
+    handalClick(box);
+  });
+});
