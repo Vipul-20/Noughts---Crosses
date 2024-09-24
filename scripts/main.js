dd7efca7ba65a0b150327d12adf18dade34b3fbd
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-09-24 14:32:26 +0000

    some function add to change the player

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
+import chances from "./chances.js";
+// console.log(chances);
+
 const headingEl = document.querySelector(".heading");
-const currentPlayer = document.querySelector(".player-stutus");
+const currPlayerEl = document.querySelector(".current-player");
 const boxes = document.querySelectorAll(".box");
 const resetBtn = document.querySelector(".btn-reset");
 
 // ======Game Variable=====
 const players = ["O", "X"];
-let currPlayer = "O";
+let currPlayer;
+let virtualgrid;
 
 function getPlayer() {
   const rn = Math.floor(Math.random() * 2);
@@ -13,18 +17,39 @@ function getPlayer() {
 }
 
 function swapPlayer() {
-  currPlayer = currPlayer === "O" ? "X" : "O";
-}
-
-// only the first one
-function handalClick(input) {
-  input.textContent = currPlayer;
-  swapPlayer();
+  const newPlayer = currPlayer === "O" ? "X" : "O";
+  currPlayer = newPlayer;
   currPlayerEl.textContent = currPlayer;
 }
 
-boxes.forEach(function (box) {
-  box.addEventListener("click", function () {
-    handalClick(box);
+// ===start game====
+function startGame() {
+  currPlayer = getPlayer();
+  virtualgrid = new Array(9).fill("");
+  currentPlayer.textContent = currPlayer;
+}
+
+function handleBoxClick(input) {
+  if (virtualgrid[input] === "") {
+    console.log("input");
+    boxes[input].textContent = currPlayer;
+    boxes[input].style.cursor = "auto";
+    virtualgrid[input] = currPlayer;
+    console.log(virtualgrid);
+    swapPlayer();
+  }
+}
+
+// console.log("boxs");
+boxes.forEach(function (_, index) {
+  _.addEventListener("click", function () {
+    handleBoxClick(index);
   });
 });
+
+function handleReset() {
+  alert("hello");
+}
+resetBtn.addEventListener("click", handleReset);
+
+document.addEventListener("DOMContentLoaded", startGame);
